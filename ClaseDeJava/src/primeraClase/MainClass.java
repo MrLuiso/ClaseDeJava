3b51ec229cc3f3224408afbd7f12bfa8b502b34a
Luis Diaz <diaz.luispr@gmail.com>
2016-08-22 12:42:21 -0400

    Testing

--- ClaseDeJava/src/primeraClase/MainClass.java
+++ ClaseDeJava/src/primeraClase/MainClass.java
@@ -1,9 +1,17 @@
 package primeraClase;
 
+import java.util.Scanner;
+
 public class MainClass {
 	
 	public static void main(String[] args){
 		
+		double PI = Math.PI;
+		double area, circunference;
+		double radius;
+		
+		System.out.println("De el radio del circulo:");
+		Scanner in = ;
 	}
 
 }
