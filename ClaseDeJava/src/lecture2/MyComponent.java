b70f2104d737a4e52fa9429f40b0f6f44844cc12
Luis Diaz <diaz.luispr@gmail.com>
2016-08-25 14:13:51 -0400

    Overriding paintComponent

--- ClaseDeJava/src/lecture2/MyComponent.java
+++ ClaseDeJava/src/lecture2/MyComponent.java
@@ -11,6 +11,7 @@ public class MyComponent extends JComponent {
 	//Generated serial id
 	private static final long serialVersionUID = 3568029827560390545L;
 
+	@Override
 	public void paintComponent(Graphics g){
 		
 		Rectangle firstRectangle = new Rectangle(10, 10, 50, 50);
