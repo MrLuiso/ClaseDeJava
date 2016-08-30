aa622c9acc7ff3d451226977a3f6028adc55f91c
Luis Diaz <diaz.luispr@gmail.com>
2016-08-30 15:09:28 -0400

    Four Cars

--- ClaseDeJava/src/lecture2/MyComponent.java
+++ ClaseDeJava/src/lecture2/MyComponent.java
@@ -14,9 +14,16 @@ public class MyComponent extends JComponent {
 	public void paintComponent(Graphics g){
 		
 		
-		Car firstCar = new Car(getWidth()-60, getHeight()-30);
+		Car firstCar = new Car(getWidth()-60, getHeight() - 30);
+		Car bottomLeftCar = new Car(0, getHeight() - 30);
+		Car topLeftCar	= new Car(0, 0);
+		Car topRightCar = new Car(getWidth() - 60, 0);
 		
 		firstCar.draw(g);
+		bottomLeftCar.draw(g);
+		topLeftCar.draw(g);
+		topRightCar.draw(g);
+		
 		
 		
 	}
