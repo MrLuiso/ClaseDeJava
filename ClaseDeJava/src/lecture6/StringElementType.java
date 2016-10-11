53813ed9cfcc25393a0cb84e2088bf6702bde3ba
Luis Diaz <diaz.luispr@gmail.com>
2016-10-11 14:28:45 -0400

    Adding String as a StringElementType

---
+++ ClaseDeJava/src/lecture6/StringElementType.java
@@ -0,0 +1,24 @@
+package lecture6;
+
+public class StringElementType extends ElementType {
+	
+	private String theString;
+	
+	public boolean greaterThan(ElementType e){
+		StringElementType eString = (StringElementType) e;
+		
+		return (this.theString.compareTo(eString.theString) > 0);
+	}
+	
+	public long valueOf(){
+		return this.theString.length();
+	}
+	
+	public boolean equals(ElementType e){
+		
+		StringElementType eString = (StringElementType) e;
+		
+		return (this.theString.equals(eString));
+	}
+
+}
